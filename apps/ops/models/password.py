e6f3dfb1bd91f1b41028a26261c5be8a86cd87df
BaiJiangJie <bugatti_it@163.com>
2019-01-10 13:58:22 +0800

    [Feature] 批量改密 - 7.优化改密任务(2)

--- apps/ops/models/password.py
+++ apps/ops/models/password.py
@@ -18,7 +18,21 @@ from ..inventory import JMSInventory
 
 logger = get_logger(__file__)
 signer = get_signer()
-TASK_NAME_CHANGE_ASSET_PASSWORD = 'change_asset_password'
+
+
+class RunAsUser:
+    def __init__(self, name, username, password):
+        self.name = name
+        self.username = username
+        self.password = password
+
+    def _to_secret_json(self):
+        """ Use when check bulk change asset password is valid"""
+        return {
+            'name': self.name,
+            'username': self.username,
+            'password': self.password,
+        }
 
 
 class ChangeAssetPasswordTask(OrgModelMixin):
@@ -34,20 +48,6 @@ class ChangeAssetPasswordTask(OrgModelMixin):
     def __str__(self):
         return self.name
 
-    @staticmethod
-    def get_tasks(username, password):
-        tasks = list()
-        tasks.append({
-            'name': TASK_NAME_CHANGE_ASSET_PASSWORD,
-            'action': {
-                'module': 'user',
-                'args': 'name={} password={} update_password=always'.format(
-                    username, encrypt_password(password, salt="K3mIlKK")
-                ),
-            }
-        })
-        return tasks
-
     def run(self, record=True):
         if record:
             return self._run_and_record()
@@ -58,8 +58,7 @@ class ChangeAssetPasswordTask(OrgModelMixin):
         history = ChangeAssetPasswordTaskHistory(task=self)
         try:
             history.date_start = timezone.now()
-            results = self._run_only()
-            result = self.handle_results(results)
+            result = self._run_only()
             history.result = result
             return result
         except Exception as e:
@@ -71,65 +70,126 @@ class ChangeAssetPasswordTask(OrgModelMixin):
             history.save()
 
     def _run_only(self):
-        results = []
-        hosts = clean_hosts(self.hosts.all())
-        if not hosts:
-            return {}
-        for host in hosts:
-            task_name = _('Change user <{}> password to asset <{}>.'
-                          .format(self.username, host))
-            password = random_password_gen()
-            tasks = self.get_tasks(self.username, password)
-            inventory = JMSInventory(assets=[host], run_as_admin=True)
-            runner = AdHocRunner(inventory, options=const.TASK_OPTIONS)
-            try:
-                result = runner.run(
-                    tasks=tasks,
-                    pattern='all',
-                    play_name=task_name
-                )
-            except AnsibleError as e:
-                logger.warn('Failed run adhoc {}, {}'.format(task_name, e))
-            else:
-                result.results_summary.update({
-                    'password': password,
-                    'hostname': host.hostname,
-                })
-                results.append((result.results_raw, result.results_summary))
-
-        return results
-
-    def handle_results(self, results):
+        """
         result = {
             'count': {
-                'total': len(results),
-                'success': 0,
-                'failed': 0,
+                'total': 0, 'success': 0, 'failed': 0,
             },
             'detail': {
-                'failed': {},
-                'success': {}
-            }
+                'failed': [], 'success': [{'hostname': 'xxx', 'msg': ''},]
+            },
+            'msg': 'Success',
         }
-        success = failed = 0
-        for raw, summary in results:
-            hostname = summary.get('hostname')
-            if summary.get('success'):
-                success += 1
-                result['detail']['success'].update({hostname: ''})
-                password = summary.get('password')
-                asset = self.hosts.all().filter(hostname=hostname).first()
-                AuthBook.create_item(self.username, password, asset)
+        :return: result
+        """
+        result = {
+            'count': {'total': 0, 'success': 0, 'failed': 0},
+            'detail': {'success': [], 'failed': []},
+            'msg': 'Success',
+        }
+
+        count_success = count_failed = 0
+        detail_success = detail_failed = []
+
+        if self.username == 'root':
+            msg = _('Change <{}> password is not allowed'.format(self.username))
+            logger.debug(msg)
+            result.update({'msg': msg})
+            return result
+
+        hosts = clean_hosts(self.hosts.all())
+        if not hosts:
+            msg = _('Change <{}> password but assets is empty.'.format(self.username))
+            logger.debug(msg)
+            result.update({'msg': msg})
+            return result
+
+        for host in hosts:
+            password = random_password_gen()
+            change_result = self._change_password_a_asset(password, host)
+
+            if self.is_success(change_result):
+                verify_result = self._verify_password_a_asset(password, host)
+
+                if self.is_success(verify_result):
+                    count_success += 1
+                    AuthBook.create_item(self.username, password, host)
+                    msg = 'Change and verify password success: {}'
+                    detail_success.append({'hostname': host.hostname, 'msg': msg})
+                else:
+                    count_failed += 1
+                    error = self.get_dark_msg(verify_result, host.hostname, 'ping')
+                    msg = 'Verify password failed: {}'.format(error)
+                    detail_failed.append({'hostname': host.hostname, 'msg': msg})
             else:
-                failed += 1
-                msg = summary.\
-                    get('dark').\
-                    get(hostname).\
-                    get(TASK_NAME_CHANGE_ASSET_PASSWORD).\
-                    get('msg')
-                result['detail']['failed'].update({hostname: msg})
-        result['count'].update({'success': success, 'failed': failed})
+                count_failed += 1
+                error = self.get_dark_msg(change_result, host.hostname, 'change_password')
+                msg = 'Change password failed: {}'.format(error)
+                detail_failed.append({'hostname': host.hostname, 'msg': msg})
+
+        result['count']['total'] = len(hosts)
+        result['count']['success'] = count_success
+        result['count']['success'] = count_failed
+        result['detail']['success'] = detail_success
+        result['detail']['failed'] = detail_failed
         return result
+
+    @staticmethod
+    def is_success(result):
+        if result and result.results_summary.get('contacted'):
+            return True
+        return False
+
+    @staticmethod
+    def get_dark_msg(result, hostname, task_name):
+        try:
+            dark = result.results_summary.get('dark')
+            msg = dark.get(hostname).get(task_name).get('msg')
+            return msg
+        except Exception as e:
+            logger.error('Get dark msg error: {}'.format(e))
+            return ''
+
+    def get_change_password_tasks(self, password):
+        tasks = list()
+        tasks.append({
+            'name': 'change_password',
+            'action': {
+                'module': 'user',
+                'args': 'name={} password={} update_password=always'.format(
+                    self.username, encrypt_password(password, salt="K3mIlKK")
+                ),
+            }
+        })
+        return tasks
+
+    def _change_password_a_asset(self, password, host):
+        task_name = _('Change <{}> password to asset <{}>'.format(self.username, host))
+        tasks = self.get_change_password_tasks(password)
+        inventory = JMSInventory(assets=[host], run_as_admin=True)
+        return self._run_task(inventory, tasks, task_name)
+
+    def _verify_password_a_asset(self, password, host):
+        task_name = _('Check <{}> password to asset <{}>'.format(self.username, host))
+        tasks = const.TEST_SYSTEM_USER_CONN_TASKS
+        run_as = RunAsUser(self.username, self.username, password)
+        inventory = JMSInventory(assets=[host], run_as=run_as)
+        return self._run_task(inventory, tasks, task_name)
+
+    @staticmethod
+    def _run_task(inventory, tasks, task_name):
+        runner = AdHocRunner(inventory, options=const.TASK_OPTIONS)
+        try:
+            result = runner.run(
+                tasks=tasks,
+                pattern='all',
+                play_name=task_name
+            )
+        except AnsibleError as e:
+            logger.warn('Failed run adhoc {}, {}'.format(task_name, e))
+            return None
+        else:
+            return result
 
 
 class ChangeAssetPasswordTaskHistory(models.Model):
